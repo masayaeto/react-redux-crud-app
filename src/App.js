be4524014a9505071bd960e46f5d1c8ea82f4388
衞藤勝也 <masaya@etomasauyanoMacBook-Pro.local>
2021-01-29 06:46:41 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("Yhea!!")}}/>
-    </React.Fragment>
-  );
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("Yhea!!")}}/>
+      </React.Fragment>
+    );
+  }
 }
+
+// const App = () => {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("Yhea!!")}}/>
+//     </React.Fragment>
+//   );
+// }
 
 export default App;
